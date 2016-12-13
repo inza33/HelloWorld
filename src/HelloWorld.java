72fca9d66ca665f9f55de731cddac5e7ac3203c8
Gosia B <a@gmail.com>
2016-12-13 19:35:49 +0100

    Utworzenie projektu

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created by lenovo on 2016-12-13.
+ */
+//komentarz
+/*komentarz*/
+//TODO
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(233);
+        System.out.println('M' + 'K');
+    }
+
+}
